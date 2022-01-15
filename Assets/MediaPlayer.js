d714fcdb3971b297f039cf0af3e1d439202029b0
JBxss <juanseb100@gmail.com>
2022-01-14 19:16:59 -0500

    Pequeñas mejoras implementando getter y setter

--- Assets/MediaPlayer.js
+++ Assets/MediaPlayer.js
@@ -1,50 +1,60 @@
 function MediaPlayer(config) {
-    this.media = config.el;
-    this.plugins = config.plugins || [];
+  this.media = config.el;
+  this.plugins = config.plugins || [];
 
-    this._initPlugins();
-  }
-
-  MediaPlayer.prototype._initPlugins = function() {
-      this.plugins.forEach(plugin => {
-          plugin.run(this);
-      });
-  }
-  
-  MediaPlayer.prototype.play = function() {
-  this.media.play();
-  }
-  
-  MediaPlayer.prototype.pause = function() {
-      this.media.pause();
-      }
-  
-  MediaPlayer.prototype.togglePlay = function () {
-    if (this.media.paused) {
-      this.play();
-    } else {
-      this.pause();
-    }
-  }
-
-  MediaPlayer.prototype.mute = function() {
-      this.media.muted = true;
-  }
-
-  MediaPlayer.prototype.unmute = function() {
-    this.media.muted = false;
+  this._initPlugins();
 }
 
-MediaPlayer.prototype.toggleMute = function () {
-    // if (this.media.muted == false) {
-    //   this.mute();
-    // } else {
-    //   this.unmute();
-    // }
+MediaPlayer.prototype._initPlugins = function () {
+  const player = {
+    play: () => this.play(),
+    pause: () => this.pause(),
+    media: this.media,
+    get muted() {
+      return this.media.muted;
+    },
+    set muted(value) {
+      this.media.muted = value;
+    },
+  };
 
-    this.media.muted ? this.unmute() : this.mute();
-    
+  this.plugins.forEach((plugin) => {
+    plugin.run(player);
+  });
+};
+
+MediaPlayer.prototype.play = function () {
+  this.media.play();
+};
+
+MediaPlayer.prototype.pause = function () {
+  this.media.pause();
+};
+
+MediaPlayer.prototype.togglePlay = function () {
+  if (this.media.paused) {
+    this.play();
+  } else {
+    this.pause();
   }
+};
 
+MediaPlayer.prototype.mute = function () {
+  this.media.muted = true;
+};
 
-  export default MediaPlayer;
+MediaPlayer.prototype.unmute = function () {
+  this.media.muted = false;
+};
+
+MediaPlayer.prototype.toggleMute = function () {
+  // if (this.media.muted == false) {
+  //   this.mute();
+  // } else {
+  //   this.unmute();
+  // }
+
+  this.media.muted ? this.unmute() : this.mute();
+};
+
+export default MediaPlayer;
--- Assets/index.js
+++ Assets/index.js
@@ -2,7 +2,7 @@ import MediaPlayer from "./MediaPlayer.js";
 import AutoPlay from "./Plugins/AutoPlay.js";
 
 const video = document.querySelector("video");
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay] });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
 
 const buttonPlay = document.querySelector("#btnPlay");
 const buttonMute = document.querySelector("#btnMute");
