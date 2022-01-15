c42067f418868bf8d42eb42db34d2da9d70db7c2
JBxss <juanseb100@gmail.com>
2022-01-14 22:28:00 -0500

    Adicion de un nuevo plugin: AutoPause, usando la API de IntersectionObserver

--- Assets/index.js
+++ Assets/index.js
@@ -1,8 +1,12 @@
 import MediaPlayer from "./MediaPlayer.js";
 import AutoPlay from "./Plugins/AutoPlay.js";
+import AutoPause from "./Plugins/AutoPause.js";
 
 const video = document.querySelector("video");
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay(), new AutoPause()],
+});
 
 const buttonPlay = document.querySelector("#btnPlay");
 const buttonMute = document.querySelector("#btnMute");
