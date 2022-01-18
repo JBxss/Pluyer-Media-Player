f6623cdf01a19ea68463b66c4b683bcaff2d0bb5
JBxss <juanseb100@gmail.com>
2022-01-18 11:18:37 -0500

    Adicion de ServiceWorkers y mejoras en el rendimiento

--- Assets/Index.js
+++ Assets/Index.js
@@ -12,3 +12,10 @@ const buttonPlay = document.querySelector("#btnPlay");
 const buttonMute = document.querySelector("#btnMute");
 buttonPlay.onclick = () => player.togglePlay();
 buttonMute.onclick = () => player.toggleMute();
+
+// Source index.js para llamar el Service Worker
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('/sw.js').catch(error =>{
+    console.log(error.message);
+  });
+}
---
+++ sw.js
@@ -0,0 +1,52 @@
+// Source sw.js
+const VERSION = "V1";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(precache());
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request; 
+  // Only GET
+  if (request.method !== "GET") {
+    return;
+  }
+
+  // buscamos en cache
+  event.respondWith(cachedResponse(request));
+
+  // actualizamos el cache
+  event.waitUntil(updateCache(request));
+});
+
+async function precache() {
+  const cache = await caches.open(VERSION);
+  return cache.addAll([
+    "./",
+    "./Index.html",
+    "./Assets/Index.js",
+    "./Assets/MediaPlayer.js",
+    "./Assets/Plugins/AutoPlay.js",
+    "./Assets/Plugins/AutoPause.js",
+    "./Assets/Index.css",
+    "./Assets/BigBuckBunny.mp4",
+  ]);
+}
+
+async function cachedResponse(request) {
+  const cache = await caches.open(VERSION);
+  const response = await cache.match(request);
+  return response || fetch(request);
+}
+
+
+// it updates the cache given a request
+async function updateCache(request) {
+  const cache = await caches.open(VERSION);
+  const response = await fetch(request);
+  // Cache.put when status code is 200 
+  // Otherwise return a simple promise 
+  return response.status === 200 
+      ? cache.put(request, response) 
+      : new Promise((resolve, reject) => resolve(':D'));    
+}
